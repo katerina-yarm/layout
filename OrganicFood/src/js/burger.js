2714d085d37be4b9abba4eed75d525ebe8c47e04
katerina-yarm <katerina.tatarevich@gmail.com>
2022-05-27 18:26:26 +0300

    Gulp settings were added to Organicfood

---
+++ OrganicFood/src/js/burger.js
@@ -0,0 +1,9 @@
+document.getElementsByClassName('icon-menu')[0].addEventListener('click', function addClass() {
+  if (document.getElementsByClassName('icon-menu')[0].classList.contains('_active')) {
+    document.getElementsByClassName('icon-menu')[0].classList.remove('_active')
+    document.getElementsByClassName('menu__body')[0].classList.remove('_active')
+  } else {
+    document.getElementsByClassName('icon-menu')[0].classList.add('_active')
+    document.getElementsByClassName('menu__body')[0].classList.add('_active')
+  }
+})
