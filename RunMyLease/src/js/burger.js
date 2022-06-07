6a3850d78417bd82abc3c5197ddb8abc93c8b6ac
katerina-yarm <katerina.tatarevich@gmail.com>
2022-06-07 13:08:29 +0300

    White theme for header is done

--- RunMyLease/src/js/burger.js
+++ RunMyLease/src/js/burger.js
@@ -1,9 +1,14 @@
-document.getElementsByClassName('icon-menu')[0].addEventListener('click', function addClass() {
+document.getElementsByClassName('icon-menu')[0].addEventListener('click', addClass)
+document.getElementsByClassName('_products')[0].addEventListener('mousedown', addClass)
+
+function addClass() {
   if (document.getElementsByClassName('icon-menu')[0].classList.contains('_active')) {
     document.getElementsByClassName('icon-menu')[0].classList.remove('_active')
+    document.getElementsByClassName('_products')[0].classList.remove('_active')
     document.getElementsByClassName('menu__body')[0].classList.remove('_active')
   } else {
     document.getElementsByClassName('icon-menu')[0].classList.add('_active')
     document.getElementsByClassName('menu__body')[0].classList.add('_active')
+    document.getElementsByClassName('_products')[0].classList.add('_active')
   }
-})
+}
