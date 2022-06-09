294931496ce55edbce656ce48d770200fed008b6
katerina-yarm <katerina.tatarevich@gmail.com>
2022-06-09 17:45:51 +0300

    Header theme color changes

--- RunMyLease/src/js/scroll.js
+++ RunMyLease/src/js/scroll.js
@@ -1,35 +1,3 @@
-function testWebP(callback) {
-  var webP = new Image()
-  webP.onload = webP.onerror = function () {
-    callback(webP.height == 2)
-  }
-  webP.src =
-    'data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA'
-}
-
-testWebP(function (support) {
-  if (support == true) {
-    document.querySelector('body').classList.add('webp')
-  } else {
-    document.querySelector('body').classList.add('no-webp')
-  }
-})
-
-document.getElementsByClassName('icon-menu')[0].addEventListener('click', addClass)
-document.getElementsByClassName('_products')[0].addEventListener('mousedown', addClass)
-
-function addClass() {
-  if (document.getElementsByClassName('icon-menu')[0].classList.contains('_active')) {
-    document.getElementsByClassName('icon-menu')[0].classList.remove('_active')
-    document.getElementsByClassName('_products')[0].classList.remove('_active')
-    document.getElementsByClassName('menu__body')[0].classList.remove('_active')
-  } else {
-    document.getElementsByClassName('icon-menu')[0].classList.add('_active')
-    document.getElementsByClassName('menu__body')[0].classList.add('_active')
-    document.getElementsByClassName('_products')[0].classList.add('_active')
-  }
-}
-
 document.addEventListener(
   'scroll',
   setInterval(function () {
@@ -85,4 +53,3 @@ document.addEventListener(
   }),
   1000
 )
-
