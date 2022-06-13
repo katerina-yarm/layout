2220341809410cef47ee10c98af1e7489f7403e3
katerina-yarm <katerina.tatarevich@gmail.com>
2022-06-13 20:51:43 +0300

    Theme changes only on the main page

--- RunMyLease/dist/js/script.js
+++ RunMyLease/dist/js/script.js
@@ -35,6 +35,7 @@ document.addEventListener(
   setInterval(function () {
     let scrolled = window.pageYOffset || document.documentElement.scrollTop
     let active = document.getElementsByClassName('_products')[0].classList.contains('_active')
+    let main = document.getElementsByTagName('main')[0].classList.contains('main')
     if (scrolled >= 100) {
       document.getElementById('header').style.background = '#F6F6F6'
       document.getElementsByClassName('header__logo')[0].style.color = '#181818'
@@ -53,7 +54,7 @@ document.addEventListener(
       document.getElementsByClassName('icon-menu')[0].children[1].style.background = '#181818'
       document.getElementsByClassName('icon-menu')[0].children[2].style.background = '#181818'
     }
-    if (100 > scrolled && !active) {
+    if (100 > scrolled && !active && main) {
       document.getElementById('header').style.background = '#181818'
       document.getElementsByClassName('header__logo')[0].style.color = '#FFFFFF'
       document.getElementsByClassName('header__links')[0].style.color = '#FFFFFF'
