e4947c83980cba6343ebcd656ecba8ab34c5f5a7
katerina-yarm <katerina.tatarevich@gmail.com>
2022-06-30 15:24:19 +0300

    Main slider is perfect

--- Funiro/src/js/functions.js
+++ Funiro/src/js/functions.js
@@ -26,3 +26,13 @@ var isMobile = {
     )
   }
 }
+
+//for ibg
+function ibg() {
+  let ibg = document.querySelectorAll('.ibg')
+  for (let i = 0; i < ibg.length; i++) {
+    if (ibg[i].querySelector('img')) {
+      ibg[i].style.backgroumdImage = `url(${ibg[i].querySelector('img').getAttribute('src')})`
+    }
+  }
+}
