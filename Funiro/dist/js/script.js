f2798b1820ac5b4f2af724ed12fe66a760024308
katerina-yarm <katerina.tatarevich@gmail.com>
2022-07-06 19:50:43 +0300

    Rooms slider was done

--- Funiro/dist/js/script.js
+++ Funiro/dist/js/script.js
@@ -350,6 +350,33 @@ if (document.querySelector('.slider-main__body')) {
   })
 }
 
+if (document.querySelector('.slider-rooms__body')) {
+  new Swiper('.slider-rooms', {
+    observer: true,
+    observeParents: true,
+    slidesPerView: 'auto', //slider will not add width to slide
+    spaceBetween: 24,
+    watchOverFlow: true,
+    speed: 800,
+    loop: true,
+    loopAdditionalSlides: 5,
+    preloadImages: false,
+    parallax: true,
+
+    //Dotts
+    pagination: {
+      el: '.slider-rooms__dotts',
+      clickable: true
+    },
+
+    //Arrows
+    navigation: {
+      nextEl: '.slider-rooms .slider-arrow_next',
+      prevEl: '.slider-rooms .slider-arrow_prev'
+    }
+  })
+}
+
 /*
 для родителей спойлеров пишем атрибут data-spollers
 для заголовков споллеров пишем фтрибут data-spoller
