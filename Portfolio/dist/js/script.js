787a2e2834c478d47c510f55fd9754b9bf47ad6b
katerina-yarm <katerina.tatarevich@gmail.com>
2022-07-08 17:41:44 +0300

    Started to make layout for portfolio, header and main banner was done

---
+++ Portfolio/dist/js/script.js
@@ -0,0 +1,27 @@
+function testWebP(callback) {
+  var webP = new Image()
+  webP.onload = webP.onerror = function () {
+    callback(webP.height == 2)
+  }
+  webP.src =
+    'data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA'
+}
+
+testWebP(function (support) {
+  if (support == true) {
+    document.querySelector('body').classList.add('webp')
+  } else {
+    document.querySelector('body').classList.add('no-webp')
+  }
+})
+
+document.getElementsByClassName('icon-menu')[0].addEventListener('click', function addClass() {
+  if (document.getElementsByClassName('icon-menu')[0].classList.contains('_active')) {
+    document.getElementsByClassName('icon-menu')[0].classList.remove('_active')
+    document.getElementsByClassName('menu__body')[0].classList.remove('_active')
+  } else {
+    document.getElementsByClassName('icon-menu')[0].classList.add('_active')
+    document.getElementsByClassName('menu__body')[0].classList.add('_active')
+  }
+})
+
