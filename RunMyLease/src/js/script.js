af04639175085bbc84330c67e796d109ccda7069
katerina-yarm <katerina.tatarevich@gmail.com>
2022-07-15 14:26:18 +0300

    Rorem was added in portfolio

--- RunMyLease/src/js/script.js
+++
@@ -1,9 +0,0 @@
-@@include('webp.js')
-@@include('sendForm.js')
-@@include('contactUsForm.js')
-@@include('validateForm.js')
-@@include('popup.js')
-@@include('plusButton.js')
-@@include('burgerMenu.js')
-@@include('scroll.js')
-
