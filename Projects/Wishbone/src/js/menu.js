b75af33a902232dc38183d850df957d32707ccd5
katerina-yarm <katerina.tatarevich@gmail.com>
2022-07-26 17:03:38 +0300

    The layout is finished

--- Projects/Wishbone/src/js/menu.js
+++ Projects/Wishbone/src/js/menu.js
@@ -26,35 +26,8 @@ window.onload = function () {
       document.querySelector('.search-form').classList.remove('_active')
     }
     //products button 'Show More'
-    if (targetElement.classList.contains('products__button')) {
-      getProducts(targetElement)
-      e.preventDefault()
-    }
-    //add products to cart
-    if (targetElement.classList.contains('actions-product__button')) {
-      const productId = targetElement.closest('.item-product').dataset.pid
-      addToCart(targetElement, productId)
-      e.preventDefault()
-    }
-    //show cart list
-    if (
-      targetElement.classList.contains('actions-header__icon') ||
-      targetElement.closest('actions-header__icon')
-    ) {
-      if (document.querySelector('.cart-list').children.length > 0) {
-        document.querySelector('.cart-header').classList.add('_active')
-      }
-      e.preventDefault()
-    } else if (
-      !targetElement.closest('.cart-header') &&
-      !targetElement.classList.contains('actions-product__button')
-    ) {
-      document.querySelector('.cart-header').classList.remove('_active')
-    }
-    //delete position from cart list
-    if (targetElement.classList.contains('cart-list__delete')) {
-      const productId = targetElement.closest('.cart-list__item').dataset.cartPid
-      updateCart(targetElement, productId, false)
+    if (targetElement.classList.contains('projects__button')) {
+      getProjects(targetElement)
       e.preventDefault()
     }
   }
@@ -80,15 +53,14 @@ function removeClass(items, className) {
 }
 
 //load more products
-async function getProducts(button) {
+async function getProjects(button) {
   if (!button.classList.contains('_hold')) {
     button.classList.add('_hold')
-    const file = 'json/products.json'
-    console.log(file)
+    const file = 'json/projects.json'
     let response = await fetch(file, { method: 'GET' })
     if (response.ok) {
       let result = await response.json()
-      loadProducts(result)
+      loadProjects(result)
       button.classList.remove('_hold')
       button.remove()
     } else {
@@ -97,157 +69,32 @@ async function getProducts(button) {
   }
 }
 
-function loadProducts(data) {
-  const productsItems = document.querySelector('.products__items')
+function loadProjects(data) {
+  const projectItems = document.querySelector('.projects__items')
   let html = ''
-  data.products.forEach((item) => {
-    const productId = item.id
-    const productUrl = item.url
-    const productImage = item.image
-    const productTitle = item.title
-    const productText = item.text
-    const productPrice = item.price
-    const productOldPrice = item.priceOld
-    const productShareUrl = item.shareUrl
-    const productLikeUrl = item.likeUrl
-    const productLabels = item.labels
+  data.projects.forEach((item) => {
+    const projectId = item.id
+    const projectUrl = item.url
+    const projectImage = item.image
+    const projectTitle = item.title
+    const projectText = item.text
 
-    html += `<article data-pid="${productId}" class="products__item item-product">`
-    if (productLabels) {
-      html += `<div class="item-product__labels">`
-
-      productLabels.forEach((label) => {
-        html += `<div class="item-product__label item-product__label_${label.type}">${label.value}</div>`
-      })
-      html += `</div>`
-    }
-
-    html += `<a href="${productUrl}" class="item-product__image">
-                <img src="img/products/${productImage}" alt="product" />
-             </a>
-    <div class="item-product__body">
-      <div class="item-product__content">
-        <h5 class="item-product__title">${productTitle}</h5>
-        <p class="item-product__text">${productText}</p>
-      </div>
-      <div class="item-product__prices">
-        <div class="item-product__price">${productPrice}</div>`
-
-    if (productOldPrice) {
-      html += `<div class="item-product__price item-product__price_old">${productOldPrice}</div>`
-    }
-
-    html += `</div>
-      <div class="item-product__actions actions-product">
-        <div class="actions-product__body">
-          <a href="" class="actions-product__button btn btn_white">Add to cart</a>
-          <div class="actions-product__links">
-            <a href="${productShareUrl}" class="actions-product__link _icon-share">Share</a>
-            <a href="${productLikeUrl}" class="actions-product__link _icon-favorite">Like</a>
+    html += `
+    <article data-pid="${projectId}" class="projects__item item-projects">
+        <a href="${projectUrl}" class="item-projects__image ibg">
+          <img src="img/projects/${projectImage}" alt="project" />
+        </a>
+        <div class="item-projects__actions actions-projects">
+          <div class="actions-projects__body">
+            <div class="actions-projects__info">
+              <h3 class="actions-projects__title">${projectTitle}</h3>
+              <p class="actions-projects__text">${projectText}</p>
+            </div>
+            <a href="${projectUrl}" class="actions-projects__button btn">Read more</a>
           </div>
         </div>
-      </div>
-    </div>
-  </article>`
+      </article>`
   })
-  productsItems.insertAdjacentHTML('beforeend', html)
-}
-
-//add to cart
-function addToCart(productButton, productId) {
-  if (!productButton.classList.contains('_hold')) {
-    productButton.classList.add('_hold')
-    productButton.classList.add('_fly')
-
-    const cart = document.querySelector('.actions-header__icon')
-    const product = document.querySelector(`[data-pid="${productId}"]`)
-    const productImage = product.querySelector('.item-product__image')
-
-    const productImageFly = productImage.cloneNode(true)
-
-    const productImageFlyWidth = productImage.offsetWidth
-    const productImageFlyHeight = productImage.offsetHeight
-    const productImageFlyTop = productImage.getBoundingClientRect().top
-    const productImageFlyLeft = productImage.getBoundingClientRect().left
-
-    productImageFly.setAttribute('class', '_fly-image')
-    productImageFly.style.cssText = `
-    left: ${productImageFlyLeft}px;
-    top: ${productImageFlyTop}px;
-    width: ${productImageFlyWidth}px;
-    height: ${productImageFlyHeight}px;
-    `
-
-    document.body.appendChild(productImageFly)
-
-    const cartFlyLeft = cart.getBoundingClientRect().left
-    const cartFlyTop = cart.getBoundingClientRect().top
-
-    productImageFly.style.cssText = `
-    left: ${cartFlyLeft}px;
-    top: ${cartFlyTop}px;
-    width: 0px;
-    height: 0px;
-    opacity: 0;
-    `
-
-    productImageFly.addEventListener('transitionend', function () {
-      if (productButton.classList.contains('_fly')) {
-        productImageFly.remove()
-        updateCart(productButton, productId)
-        productButton.classList.remove('_fly')
-      }
-    })
-  }
-}
-
-function updateCart(productButton, productId, productAdd = true) {
-  const cart = document.querySelector('.cart-header')
-  const cartIcon = cart.querySelector('.actions-header__icon')
-  const cartQuantity = cartIcon.querySelector('span')
-  const cartProduct = document.querySelector(`[data-cart-pid="${productId}"]`)
-  const cartList = document.querySelector('.cart-list')
-
-  if (productAdd) {
-    if (cartQuantity) {
-      cartQuantity.innerHTML = ++cartQuantity.innerHTML
-    } else {
-      cartIcon.insertAdjacentHTML('beforeend', `<span>1</span>`)
-    }
-    if (!cartProduct) {
-      const product = document.querySelector(`[data-pid="${productId}"]`)
-      const cartProductImage = product.querySelector('.item-product__image').innerHTML
-      const cartProductTitle = product.querySelector('.item-product__title').innerHTML
-      const cartProductContent = `
-      <a href='' class='cart-list__image'>${cartProductImage}</a>
-      <div class='cart-list__body'>
-      <a href='' class='cart-list__title'>${cartProductTitle}</a>
-      <div  class='cart-list__quantity'>Quantity: <span>1</span></div>
-      <a href='' class='cart-list__delete'>Delete</a>
-      </div>
-      `
-      cartList.insertAdjacentHTML(
-        'beforeend',
-        `<li data-cart-pid='${productId}' class='cart-list__item'>${cartProductContent}</li>`
-      )
-    } else {
-      const cartProductQuantity = cartProduct.querySelector('.cart-list__quantity span')
-      cartProductQuantity.innerHTML = ++cartProductQuantity.innerHTML
-    }
-    productButton.classList.remove('_hold')
-  } else {
-    const cartProductQuantity = cartProduct.querySelector('.cart-list__quantity span')
-    cartProductQuantity.innerHTML = --cartProductQuantity.innerHTML
-    if (!parseInt(cartProductQuantity.innerHTML)) {
-      cartProduct.remove()
-    }
-
-    const cartQuantityValue = --cartQuantity.innerHTML
-    if (cartQuantityValue > 0) {
-      cartQuantity.innerHTML = cartQuantityValue
-    } else {
-      cartQuantity.remove()
-      cart.classList.remove('_active')
-    }
-  }
+  projectItems.insertAdjacentHTML('beforeend', html)
+  ibg()
 }
