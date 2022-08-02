cbb68c68f78539d800da5cd6a8b5ed070a9802b6
katerina-yarm <katerina.tatarevich@gmail.com>
2022-08-02 15:50:29 +0300

    Slider with new products was done

--- Projects/Romsem/src/js/sliders.js
+++ Projects/Romsem/src/js/sliders.js
@@ -1,5 +1,5 @@
 if (document.querySelector('.main-slider__body')) {
-  new Swiper('.swiper', {
+  new Swiper('.main-slider__swiper', {
     slidesPerView: 'auto',
     //watchOverFlow: true,
     loop: true,
@@ -8,6 +8,28 @@ if (document.querySelector('.main-slider__body')) {
     pagination: {
       el: '.controls-slider-main__dotts',
       clickable: true
+    }
+  })
+}
+
+if (document.querySelector('.products-slider__body')) {
+  new Swiper('.products-slider__swiper', {
+    observer: true,
+    observeParents: true,
+    slidesPerView: "auto", //slider will not add width to slide
+    spaceBetween: 30,
+    watchOverFlow: true,
+    speed: 800,
+    loop: true,
+    loopAdditionalSlides: 5,
+    preloadImages: false,
+    parallax: true,
+    centeredSlides: true,
+
+    //Arrows
+    navigation: {
+      nextEl: '.products-slider .slider-arrow_next',
+      prevEl: '.products-slider .slider-arrow_prev'
     }
   })
 }
