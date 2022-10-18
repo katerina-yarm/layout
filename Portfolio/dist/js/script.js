31659f0fd5bc8602a3cfb3976ccef5fdc27d54bd
katerina-yarm <katerina.tatarevich@gmail.com>
2022-10-18 17:22:22 +0300

    Some changes were addede to burger

--- Portfolio/dist/js/script.js
+++ Portfolio/dist/js/script.js
@@ -37,15 +37,29 @@ if (document.querySelector('.portfolio-slider__body')) {
   })
 }
 
-document.getElementsByClassName('icon-menu')[0].addEventListener('click', function addClass() {
-  if (document.getElementsByClassName('icon-menu')[0].classList.contains('_active')) {
-    document.getElementsByClassName('icon-menu')[0].classList.remove('_active')
-    document.getElementsByClassName('menu__body')[0].classList.remove('_active')
-  } else {
-    document.getElementsByClassName('icon-menu')[0].classList.add('_active')
-    document.getElementsByClassName('menu__body')[0].classList.add('_active')
+window.onload = function () {
+  document.addEventListener('click', documentActions)
+
+  function documentActions(e) {
+    const targetElement = e.target
+    console.log(targetElement)
+    if (targetElement.classList.contains('icon-menu') || targetElement.closest('.icon-menu')) {
+      if (document.getElementsByClassName('icon-menu')[0].classList.contains('_active')) {
+        document.getElementsByClassName('icon-menu')[0].classList.remove('_active')
+        document.getElementsByClassName('menu__body')[0].classList.remove('_active')
+      } else {
+        document.getElementsByClassName('icon-menu')[0].classList.add('_active')
+        document.getElementsByClassName('menu__body')[0].classList.add('_active')
+      }
+    } else if (
+      targetElement.classList.contains('menu__list') ||
+      document.getElementsByClassName('menu__body')[0].classList.contains('_active')
+    ) {
+      document.getElementsByClassName('icon-menu')[0].classList.remove('_active')
+      document.getElementsByClassName('menu__body')[0].classList.remove('_active')
+    }
   }
-})
+}
 
 /*
 для родителей спойлеров пишем атрибут data-spollers
